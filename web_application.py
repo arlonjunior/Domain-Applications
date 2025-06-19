b25238674c571d5e9f8c84a25f4bd435748e393e
arlonjunior <arlonmoleka@gmail.com>
2025-06-19 22:07:40 +0100

    Merge remote-tracking branch 'origin/master'

--- web_application.py
+++ web_application.py
@@ -156,7 +156,10 @@ elif aggregation == "Sales per Year":
 
 elif aggregation == "Sales per Customer":
     sales_data = df_filtered.groupby("CustomerID", as_index=False)["TotalPrice"].sum().sort_values(by="TotalPrice", ascending=False)
-    fig = px.line(sales_data.head(20), x="CustomerID", y="TotalPrice", title="Top 20 Customers by Sales", labels={"TotalPrice": "Sales (£)"})
+    fig = px.treemap(sales_data.head(20), path=["CustomerID"], values="TotalPrice",
+                     title="Top 20 Customers by Sales",
+                     color="TotalPrice",
+                     color_continuous_scale="Blues")
     st.plotly_chart(fig, use_container_width=True)
     st.dataframe(sales_data)
 
